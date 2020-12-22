f92e12d4c1ada6195ea5e0879db7a9c8a5dee705
HamedMirza <hamedmirzaa1@gmail.com>
2020-12-22 17:46:29 -0500

    Section 22 progres

---
+++ Hameds Code/tripleX.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ // Print introduction to the prompt
+ std::cout << "Welcome to the trial of the sacred doors";
+ std::cout << std::endl;
+ std::cout << "Enter the mystical code to unlock the door and move on to the next trial.";
+  
+  // Declare 3 number code
+  std::cout << std::endl;
+  int a = 6;
+  int b = 2;
+  int c = 9;
+  int sum = a + b + c;
+  int product = a * b * c;
+  
+  // Print results
+  std::cout << std::endl;
+  std::cout << "There are 3 numbers in the code!";
+  std::cout << std::endl;
+  std::cout << "The sum of the codes is" << sum;
+  std::cout << std::endl;
+  std::cout << "The product of the codes is" << product;
+  return 0;
+}
