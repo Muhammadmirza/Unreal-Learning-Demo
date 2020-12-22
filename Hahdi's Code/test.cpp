a6f14d58e5542ea4555e3e2aa2de54ead33a3788
HahdiMirza <hahdi.mirza@gmail.com>
2020-12-22 17:16:19 -0500

    added gitignore

--- Hahdi's Code/test.cpp
+++ Hahdi's Code/test.cpp
@@ -1,1 +1,22 @@
 //Hahdi will code in here.
+
+#include <iostream>
+#include <ctime>
+
+void PlayGameAtDifficulty(int difficulty){
+    std::cout <<"skip level.";
+}
+
+int main() {
+    int difficulty = 2;
+    int maxDifficulty = 10;
+    while (difficulty <= maxDifficulty) {
+        PlayGameAtDifficulty(difficulty);
+        std::cin.clear(); //cleras the failbit
+        std::cin.ignore(); //discards teh buffer
+        ++difficulty;
+    }
+
+    std::cout << "WOW - You won. You're really good at this!\n";
+    return 0; //exit with no error code
+}
