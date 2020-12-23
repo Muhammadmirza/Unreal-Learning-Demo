a1b1c905161010609b34909212aef572516d1a6f
HamedMirza <hamedmirzaa1@gmail.com>
2020-12-22 19:05:40 -0500

    test

--- Hameds Code/tripleX.cpp
+++ Hameds Code/tripleX.cpp
@@ -4,25 +4,24 @@ using namespace std;
 int main()
 {
  // Print introduction to the prompt
- std::cout << "Welcome to the trial of the sacred doors";
- std::cout << std::endl;
- std::cout << "Enter the mystical code to unlock the door and move on to the next trial.";
+ std::cout << "Welcome to the trial of the sacred doors \n";
+ std::cout << "Enter the mystical code to unlock the door and move on to the next trial.\n";
   
   // Declare 3 number code
-  std::cout << std::endl;
   const int CodeA = 1;
   const int CodeB = 2;
   const int CodeC = 3;
   const int CodeSum = CodeA + CodeB + CodeC;
   const int CodeProduct = CodeA * CodeB * CodeC;
   
-  // Print results
-  std::cout << std::endl;
-  std::cout << "There are 3 numbers in the code!" << std::endl;
-  std::cout << "The sum of the codes is: " << CodeSum << std::endl;
-  std::cout << "The product of the codes is: " << CodeProduct << std::endl;
+  // Print hints
+  std::cout << "\n There are 3 numbers in the code!";
+  std::cout << "\n The sum of the codes is: " << CodeSum;
+  std::cout << "\n The product of the codes is: " << CodeProduct;
 
+  // Store Player Guesses
   int GuessA, GuessB, GuessC;
+  std::cout << std::endl;
   std::cin >> GuessA;
   std::cin >> GuessB;
   std::cin >> GuessC;
@@ -31,13 +30,15 @@ int main()
   int GuessSum = GuessA + GuessB + GuessC;
   int GuessProduct = GuessA + GuessB + GuessC;
 
+  // Print results
+
   if (GuessSum == CodeSum && GuessProduct == CodeProduct)
   {
-    std::cout << "The sacred spirits are pleased and have unlocked the door for your correct answer.";
+    std::cout << "\n The sacred spirits are pleased and have unlocked the door for your correct answer.";
   }
   else
   {
-    std::cout << "The sacred spirits look down upon you for your stupidity, you lose.";
+    std::cout << "\n The sacred spirits look down upon you for your stupidity, you lose.";
   }
   return 0;
 
