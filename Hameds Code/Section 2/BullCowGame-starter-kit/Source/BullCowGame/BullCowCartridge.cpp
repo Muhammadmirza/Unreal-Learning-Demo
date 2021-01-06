0771a65c417e21182de4b042750129d5e381ea29
HamedMirza <hamedmirzaa1@gmail.com>
2021-01-06 15:10:30 -0500

    test

--- Hameds Code/Section 2/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ Hameds Code/Section 2/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,12 +5,15 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
+    SetupGame(); // Setting up the game
+
+    PrintLine(TEXT("The HiddenWord is: %s."), *HiddenWord); // Debug Line, remove in final product
+
     // Player Introduction
     PrintLine(TEXT("Welcome to Bull Cows!")); // intro message to welcome player
-    PrintLine(TEXT("Guess the 4 letter word!")); // magic number
-    PrintLine(TEXT("Press enter to continue...")); // intro message to guide the player how to actually start the basic game loop
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len()); // magic number
+    PrintLine(TEXT("Type and enter your answer...")); // intro message to guide the player how to actually start the basic game loop
     
-    SetupGame(); // Setting up the game
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
@@ -19,18 +22,18 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
 
     //Checking the Player Guess
 
-    if (Input == HiddenWord)
+    if (Input == HiddenWord) // Checks if the player input matches the hiddenword
      {
-        PrintLine(TEXT("You guessed the hidden word, congrats!"));
+        PrintLine(TEXT("You guessed the hidden word, congrats!")); // Prints winning message
     }
     else 
     {
-        if (Input.Len() != HiddenWord.Len())
+        if (Input.Len() != HiddenWord.Len()) // Checks if player's inputted word is the same length as the length of our hidden word
         {
-            PrintLine(TEXT("The hidden word is 4 letters long, try again!")); // magic number
+            PrintLine(TEXT("The hidden word is %i letters long!"), HiddenWord.Len()); // magic number
         }
         
-        PrintLine(TEXT("You are incorrect, try again!"));
+        PrintLine(TEXT("You are incorrect, try again!")); // Prints losing message
     }
     // Check if its an isogram here
     // Check if the right number of character are entered
