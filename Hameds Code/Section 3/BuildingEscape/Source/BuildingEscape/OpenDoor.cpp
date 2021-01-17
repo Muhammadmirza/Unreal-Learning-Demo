924cd75a7656d6d5ba7e79e929103d7217cb6101
HamedMirza <hamedmirzaa1@gmail.com>
2021-01-17 18:59:51 -0500

    Section 104-106

--- Hameds Code/Section 3/BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ Hameds Code/Section 3/BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -20,10 +20,10 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	FRotator CurrentRotation = GetOwner()->GetActorRotation();
-	CurrentRotation.Yaw = 30.f;
-
-	GetOwner()->SetActorRotation(CurrentRotation);
+	InitialYaw = GetOwner()->GetActorRotation().Yaw;
+	CurrentYaw = InitialYaw;
+	TargetYaw = InitialYaw + 30.f;
+	
 }
 
 
@@ -32,6 +32,20 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("The Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
+
+
+	CurrentYaw = FMath::FInterpTo(CurrentYaw, TargetYaw, DeltaTime, 2.f);
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner()->SetActorRotation(DoorRotation);
+	
+	// float CurrentYaw = GetOwner()->GetActorRotation().Yaw;
+	// FRotator OpenDoor(0.f, TargetYaw, 0.f);
+	// OpenDoor.Yaw = FMath::Lerp(CurrentYaw, TargetYaw, 0.02f);
+	// GetOwner()->SetActorRotation(OpenDoor);
+
+
 }
 
--- Hameds Code/Section 3/BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ Hameds Code/Section 3/BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -24,5 +24,9 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+private:
+
+	float InitialYaw;
+	float CurrentYaw;
+	float TargetYaw ;
 };
